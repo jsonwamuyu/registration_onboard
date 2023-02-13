594659013f8cfacebe360c788035a1ca8142227b
jsonwamuyu <jysonmuchiri@gmail.com>
2023-02-13 00:21:05 -0800

    Heros CTA section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,13 @@ const Hero = () => {
         <div className='flex flex-col space-y-3 text-center sm:text-left'>
             <h6>Hi, my name is </h6>
             <h1 className='text-5xl font-bold'>Johnson Muchiri</h1>
-            <h4>Frontend Web Developer</h4>
+            <h4 className='text-xl font-semi-bold'>Frontend Web Developer</h4>
+            <p className='font-[400] text-[14px] sm:max-w-xs'>I love building web applications. 
+            Transforming a design into something beautifull</p>
+            <div className='flex space-x-6 pt-6'>
+              <a href='' className='hover:bg-[#646cff] hover:text-[#f8f8f8] rounded-[4px] px-6 py-2 border'>Let us Talk</a>
+              <a href="" className='px-6 py-2 rounded-[4px] cursor-pointer border '>Download CV</a>
+            </div>
         </div>
        <div className='flex items-center justify-center'>
         <img src="/nas.jpg" alt="" className='w-[200px] h-[200px] object-cover rounded-[50%]'/>
@@ -16,4 +22,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,34 @@
-import React from 'react'
+import {useState} from 'react'
+import {HiX, HiOutlineMenuAlt3, HiMenuAlt2, HiMenu, HiMenuAlt3} from 'react-icons/hi'
 import {BsLinkedin, BsGithub, BsTwitter, BsFacebook} from 'react-icons/bs'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <nav className='w-full bg-white h-[4rem] fixed '>
-      <div className="flex items-center drop-shadow-md justify-between mx-auto w-[96%] sm:w-[92%]  h-full md:w-[90%] lg:w-[86%]">
+    <nav className='w-full h-[4rem] fixed drop-shadow-md z-50'>
+      <div className="flex items-center justify-between mx-auto w-[96%] sm:w-[92%]  h-full md:w-[90%] lg:w-[86%]">
         <div className='flex items-center justify-center'>
-          <h2 className='text-2xl text-green-500 font-bold'>Jyson Muchiri</h2>
+          <h2 className='text-2xl text-[#646cff] font-bold'>Jyson Muchiri</h2>
         </div>
         <ul className='hidden sm:flex flex-row items-center justify-center sm:space-x-6 '>
           <li><a href="" className='font-bold'>Home</a></li>
           <li><a href="" className='font-bold'>About</a></li>
           <li><a href="" className='font-bold'>Projects</a></li>
           <li><a href="" className='font-bold'>Contact Me</a></li>
-        </ul>
-        <div className='flex flex-row space-x-2'>
+        </ul> 
+        <div className='hidden sm:flex flex-row space-x-2 sm:space-x-4 '>
           <BsLinkedin className='h-4 w-4'/>
           <BsGithub className='h-4 w-4' />
           <BsFacebook className='h-4 w-4' />
           <BsTwitter className='h-4 w-4' />
+        </div>
+
+        {/* Collapsable manu bar */}
+
+        <div className='sm:hidden flex items-center justify-center h-full'>
+          <HiMenu className='h-[50%] w-[40px]'/>
         </div>
       </div>
     </nav>
